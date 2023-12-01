251cf963ad89c6af7f85f8ac92222f693b6b95a0
dnavmoy <dnavmoy119@g.educaand.es>
2023-12-01 09:01:49 +0100

    eva se aburre

--- src/main/java/enclase/paraborrar/ParaBorrar.java
+++ src/main/java/enclase/paraborrar/ParaBorrar.java
@@ -11,6 +11,6 @@ package enclase.paraborrar;
 public class ParaBorrar {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Eva se aburre");
     }
 }
